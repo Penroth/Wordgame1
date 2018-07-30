152ff49ba9b111c5cf9a0053f005b538d34e7095
Penroth <mariuskrieger@hotmail.com>
2018-07-30 12:33:13 +0200

    Minor changes

--- Assets/Scripts/DragSceneScripts/LetterTextScript.cs
+++ Assets/Scripts/DragSceneScripts/LetterTextScript.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using NUnit.Framework.Interfaces;
+//using NUnit.Framework.Interfaces;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
